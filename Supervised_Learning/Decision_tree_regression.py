fd94416241e33d1001c0952a4ebd1b13c0855e48
Bhushan4829 <bhushanmahajan2908@gmail.com>
2025-01-23 22:39:44 -0500

    Updated Files

--- Supervised_Learning/Decision_tree_regression.py
+++ Supervised_Learning/Decision_tree_regression.py
@@ -3,5 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-training_data = pd.read_csv("Dataset\\american_express_data\\train.csv")
-print(training_data.describe())
+# training_data = pd.read_csv("D:\\Downloads\\ML_Models\\american_express_data\\train.csv")
+training_data = pd.read_csv(r"Dataset\american_express_data\train.csv")
+print(training_data.columns())
